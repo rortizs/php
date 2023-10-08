535e9d130a635ba872e7bf85e9845d1ba31f514c
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-10-08 14:42:11 -0600

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+   <?php
+   $color = "red";
+    echo "My car is " . $color . "<br>";
+   ?> 
+</body>
+</html>
