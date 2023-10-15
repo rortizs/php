022e20be20d4e0eca00a45a069642be69eacd6de
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-10-15 14:42:53 -0600

    add project

---
+++ MiPrimerProyecto/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Mostrar los errores
+ */
+
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set("error_log", "/Users/richardortiz/workspace/curso_web/php/MiPrimerProyecto/errors/php_err_log");
