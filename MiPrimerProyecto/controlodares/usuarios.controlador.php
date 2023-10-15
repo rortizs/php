03784253fa7b6efafcad2047a80cb0074e2a7120
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-10-15 16:03:51 -0600

    ajax, js, modelo usuario y controlador usuario v2

--- MiPrimerProyecto/controlodares/usuarios.controlador.php
+++ MiPrimerProyecto/controlodares/usuarios.controlador.php
@@ -8,4 +8,26 @@ class ControladorUsuarios{
         
         return $respuesta;
     }
+
+    /**
+     * FUNCTION INGRESAR USUARIO
+     */
+
+    static public function ctrIngresarUsuario(){
+
+        if(isset($_POST["ingUsuario"])){
+            if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"])){
+
+
+                $tabla = "usuarios";
+                $item = "usuario";
+                $valor = $_POST["ingUsuario"];
+                $respuesta = Modelousuarios::mdlMostrarUsuarios($tabla, $item, $valor);
+
+                //TODO: CONTINUAMOS EN LA PROX. CLASE MIERCOLES
+            }
+        }
+    }
+
+    //TODO:PENDIENTE VALIDAR NO REPETIR EL USUARIO
 }
--- MiPrimerProyecto/modelos/usuarios.modelo.php
+++ MiPrimerProyecto/modelos/usuarios.modelo.php
@@ -44,7 +44,7 @@ class ModeloUsuarios {
             return "error";
         }
 
-        //TODO:VALIDAR NO REPETIR EL USUARIO
+        //TODO:VALIDAR NO REPETIR EL USUARIO?
 
         $stmt = null;
     }
@@ -58,5 +58,45 @@ class ModeloUsuarios {
         $stmt = Conexion::conectar()->prepare("
             UPDATE $tabla SET nombre = :nombre, usuario = :usuario,
             password = :password WHERE usuario = :usuario");
+    }
+
+    /**
+     * Modelo Actualizar Usuario
+     */
+    static public function mdlActualizarUsuario($tabla, $item1, $valor1, $item2, $valor2){
+        $stmt = Conexion::conectar()->prepare("
+            UPDATE $tabla SET $item1 = :$item1 WHERE $item2 = :$item2
+        ");
+
+        $stmt->bindParam(":" . $item1, $valor1, PDO::PARAM_STR);
+        $stmt->bindParam(":" . $item2, $valor2, PDO::PARAM_STR);
+
+        if($stmt->execute()){
+            return "ok";
+        }else{
+            return "erorr";
+        }
+
+        $stmt = null;
+    }
+
+    /**
+     * Modelo Eliminar Usuario
+     */
+
+    static public function mdlEliminarUsuario($tabla, $valor){
+        $stmt = Conexion::conectar()->prepare("
+            DELETE FROM $tabla WHERE id = :id
+        ");
+
+        $stmt->bindParam(":id", $valor, PDO::PARAM_INT);
+
+        if($stmt->execute()){
+            return "ok";
+        }else{
+            return "erorr";
+        }
+
+        $stmt = null;
     }
 }
